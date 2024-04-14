4e88063a23356c193ed91b09812ce2ebd54de6e0
Gaurav <gauravhpanchal2@gmail.com>
2024-04-14 11:27:49 +0530

    faq structure changes

--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -28,7 +28,7 @@ const faq = [
 
 const FAQ = () => {
     return (
-        <div id="faq-section" className='mx-auto max-w-7xl py-24 lg:px-8 bg-faqblue rounded-2xl my-16 faq-bg'>
+        <div id="faq-section" className='mx-auto max-w-7xl py-12 lg:px-8 bg-faqblue rounded-2xl my-16 faq-bg'>
             <h3 className='text-xl font-normal text-white text-center mb-6'>FAQ</h3>
             <h2 className='text-4xl lg:text-6xl font-semibold text-center text-white'>Frequently asked <br /> questions.</h2>
             <div className="w-full px-4 pt-16 space-y-4">
@@ -38,12 +38,14 @@ const FAQ = () => {
                             <Disclosure>
                                 {({ open }) => (
                                     <>
-                                        <Disclosure.Button className="flex w-full justify-between items-center rounded-lg px-4 py-2 text-left text-2xl font-medium">
+                                        <Disclosure.Button className="flex w-full justify-between items-center rounded-lg px-4 py-2 text-left text-xl font-medium">
                                             <span>{item.question}</span>
-                                            <ChevronUpIcon
-                                                className={`${open ? 'rotate-180 transform' : ''
-                                                    } h-5 w-5 text-purple-500`}
-                                            />
+                                            <div className="w-[10%]">
+                                                <ChevronUpIcon
+                                                    className={`${open ? 'rotate-180 transform' : ''
+                                                        } h-5 w-5 text-purple-500`}
+                                                />
+                                            </div>
                                         </Disclosure.Button>
                                         <Disclosure.Panel className="px-4 pt-4 pb-2 text-base text-black font-normal opacity-50">
                                             {item.answer}

3402ffdc270f9bda5b8ac6d1cabdaf9d26fe95b4
Gaurav <gauravhpanchal2@gmail.com>
2024-04-14 13:16:45 +0530

    Errors fixed

--- app/components/Beliefs/index.tsx
+++ app/components/Beliefs/index.tsx
@@ -9,7 +9,7 @@ const Beliefs = () => {
                 <div className="bg-darkblue bg-beliefs pt-12 px-10 sm:px-24 pb-52 md:pb-70 rounded-3xl">
                     <h2 className="text-lg font-normal text-white tracking-widest mb-5 text-center sm:text-start">BELIEFS</h2>
                     <h3 className="text-4xl sm:text-65xl font-bold text-white leading-snug mb-5 text-center sm:text-start">Honesty <span className="text-grey">and hard work are our beliefs.</span></h3>
-                    <h5 className="text-offwhite pt-2 mb-5 text-center sm:text-start">Navigate Mumbai's iconic landmarks and hidden gems with comfort and ease in our cars.</h5>
+                    <h5 className="text-offwhite pt-2 mb-5 text-center sm:text-start">Navigate Mumbai&apos;s iconic landmarks and hidden gems with comfort and ease in our cars.</h5>
                     <div className="text-center sm:text-start">
                         <button className="text-xl py-5 px-14 mt-5 font-semibold text-white rounded-full bg-blue border border-blue hover:bg-hoblue">Book Now</button>
                     </div>
@@ -20,7 +20,7 @@ const Beliefs = () => {
                 <div className="bg-build pt-12 px-10 sm:px-24 pb-52 md:pb-70 rounded-3xl">
                     <h2 className="text-lg font-normal text-blue tracking-widest mb-5 text-center sm:text-start">DRIVE</h2>
                     <h3 className="text-4xl sm:text-65xl font-bold text-black leading-snug mb-5 text-center sm:text-start"><span className="text-blue">Navigate</span> Mumbai and ignite your creativity.</h3>
-                    <h5 className="bluish pt-2 mb-5 text-center sm:text-start">Experience Mumbai's best with our hassle-free car hire for your city exploration.</h5>
+                    <h5 className="bluish pt-2 mb-5 text-center sm:text-start">Experience Mumbai&apos;s best with our hassle-free car hire for your city exploration.</h5>
                     <div className="text-center sm:text-start">
                         <button className="text-xl py-5 px-14 mt-5 font-semibold text-white rounded-full bg-blue border border-blue hover:bg-hoblue">Book Now</button>
                     </div>

7ebb3ebe109daaec24eed715ae9bda0dd01c9b8c
Gaurav <gauravhpanchal2@gmail.com>
2024-04-19 01:07:00 +0530

    contacts added

--- app/components/BottomNav/index.tsx
+++ app/components/BottomNav/index.tsx
@@ -7,15 +7,15 @@ const BottomNav = () => {
         <div className="grid grid-cols-3 fixed bottom-0 left-0 right-0 border-t-[1px] border-[#1F2937] text-white font-medium text-center w-screen m-0 p-0 h-16 bg-btnblue">
             <div className="flex flex-col justify-center border-r items-center">
                 <BiMessageDetail className="h-6 w-6" />
-                <a href="tel:+91 8286112257">Send Query</a>
+                <a href="tel:+919372230856">Send Query</a>
             </div>
             <div className="flex flex-col justify-center border-r items-center">
                 <IoMdCall className="h-6 w-6" />
-                <a href="tel:+919076182397">Call us</a>
+                <a href="tel:+919372230856">Call us</a>
             </div>
             <div className="flex flex-col justify-center items-center">
                 <FaWhatsapp className="h-6 w-6" />
-                <a href="https://wa.me/919076182397?">Whatsapp</a>
+                <a href="https://wa.me/919372230856?">Whatsapp</a>
             </div>
         </div>
     );
